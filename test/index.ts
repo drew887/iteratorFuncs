57c773bb39766b3c12f688e24d5ace9f0aff8e57
Andrew Mcdonald Притула <drew887121@gmail.com>
2023-09-28 00:55:59 -0400

    init commit

---
+++ test/index.ts
@@ -0,0 +1,9 @@
+import {describe,it} from 'node:test'
+import assert from "node:assert";
+
+
+describe("example test", () => {
+    it("should succeed", () => {
+        assert.strictEqual(1,1)
+    });
+});
