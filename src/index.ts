3910c759175e1e04f3e36b7784ffa8fd96e8b5e3
Andrew Mcdonald Притула <drew887121@gmail.com>
2023-10-09 11:08:04 -0400

    fix: Adds in prettier, husky, lint-staged

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,41 @@
+//TODO: Make non greedy versions
 
+/**
+ * Given an iterator greedily reduce over its results until it finishes and return the result
+ * @param iterator
+ * @param reducer
+ * @param initial
+ */
+export function reduceIterator<T, TReturn>(
+  iterator: Iterable<T>,
+  reducer: (carry: TReturn, arg: T) => TReturn,
+  initial: TReturn,
+): TReturn {
+  let result: TReturn = initial;
 
+  for (const a of iterator) {
+    result = reducer(result, a);
+  }
 
-export function reduceIterator<T, C>(iterator: IterableIterator<T>, reducer: (carry: C, arg: T) => C, initial: C): C {
-    let result: C = initial;
+  return result;
+}
 
-    for(const a of iterator){
-        result = reducer(result, a);
-    }
+/**
+ * Greedily Map over an iterator and return an array of results
+ * @param iterator
+ * @param mapper
+ */
+export function mapIterator<T, TReturn>(
+  iterator: Iterable<T>,
+  mapper: (arg: T) => TReturn,
+): Array<TReturn> {
+  return reduceIterator(
+    iterator,
+    (carry, item): TReturn[] => {
+      carry.push(mapper(item));
 
-    return result;
+      return carry;
+    },
+    [] as TReturn[],
+  );
 }
--- test/index.ts
+++ test/index.ts
@@ -1,26 +1,71 @@
-import {beforeEach, describe, it} from "node:test";
+import { beforeEach, describe, it } from "node:test";
 import assert from "node:assert";
 
-import {reduceIterator} from "../src";
-
+import { mapIterator, reduceIterator } from "../src";
 
 describe("reduce", () => {
-    describe("plain", () => {
-        let iter: Generator<number, void>;
-        const generator = function* () {
-            yield 2;
-        };
+  describe("Generator", () => {
+    let iter: Generator<number, void>;
 
-        beforeEach(() => {
-            iter = generator();
-        });
+    const generator = function* () {
+      for (let i = 1; i <= 5; i++) {
+        yield i;
+      }
 
-        it("should map the iterator to the end", () => {
-            const actual = reduceIterator(iter as IterableIterator<number>, (carry, item) => {
-                return carry + item;
-            }, 0);
+      return;
+    };
 
-            assert.strictEqual(iter.next().done, true, "iterator marks done");
-        });
+    beforeEach(() => {
+      iter = generator();
     });
+
+    it("should map the iterator to the end", () => {
+      const actual = reduceIterator(
+        iter as IterableIterator<number>,
+        (carry, item) => {
+          return carry + item;
+        },
+        0,
+      );
+
+      assert.strictEqual(iter.next().done, true, "iterator marks done");
+      assert.strictEqual(actual, 15, "computes the right number");
+    });
+  });
+
+  describe("Iterable", () => {});
+
+  describe("IterableIterator", () => {});
+});
+
+describe("map", () => {
+  describe("generator", () => {
+    let iter: Generator<number, void>;
+
+    const generator = function* () {
+      for (let i = 1; i <= 5; i++) {
+        yield i;
+      }
+
+      return;
+    };
+
+    beforeEach(() => {
+      iter = generator();
+    });
+
+    it("should map all the elements as expected", () => {
+      const mapped = mapIterator(iter, (num) => {
+        return num + 1;
+      });
+      const expected = [2, 3, 4, 5, 6];
+
+      assert.strictEqual(iter.next().done, true, "iterator marks done");
+      assert.deepEqual(
+        mapped,
+        expected,
+        "Gets all values and correctly maps them",
+      );
+    });
+  });
 });
